d49e78a97a6153dc0e53cb69ec931026e2ed45e6
Daulet <dauletkhabidullin@gmail.com>
2023-03-26 21:25:21 +0600

    updated config file

---
+++ config_data/config.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+@dataclass
+class Tgbot:
+    token: str
+
+@dataclass
+class Config:
+    tg_bot: Tgbot
+
+
+def load_config(path: str | None = None) -> Config:
+    env: Env = Env()
+    env.read_env()
+    return Config(tg_bot=Tgbot(token=env("TOKEN_BOT")))
